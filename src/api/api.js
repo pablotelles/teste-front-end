b66967aed62d512c19fb33c160155bf3865129ef
Pablo Teles <88568500+pablotelles@users.noreply.github.com>
2022-04-27 17:46:09 -0300

    request

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://app.econverse.com.br/teste-front-end/junior/caoselheiro/lista-produtos/produtos'
+  baseURL: 'https://app.econverse.com.br/teste-front-end/junior/caoselheiro/lista-produtos/produtos.json'
 })
